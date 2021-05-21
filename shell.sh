e12bde10beb0e71e2a91f287d56dedfb1df80ee2
⚝ Pranav Kulkarni ⚝ <pranavkulkarni195@gmail.com>
2021-05-21 18:02:40 +0530

    Create shell.sh

---
+++ shell.sh
@@ -0,0 +1,8 @@
+apt-get update
+
+apt-get install zsh wget proot openssh -y
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
