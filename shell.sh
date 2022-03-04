c004910ade0d5b566defd2d0053b419840dfc65b
Pranav <38887390+thegamerhat@users.noreply.github.com>
2022-03-04 15:24:14 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,8 +1,14 @@
-apt-get update
+# Update packages (important)
+pkg up
 
-apt-get install git zsh wget proot openssh -yq
+# Install basic stuff
+pkg install git zsh wget proot openssh busybox termux-services -y
 
+# Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Powerlevel10k zsh customization
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+echo -e "/nCheck out https://wiki.termux.com/wiki/Remote_Access for ssh and ftp!"
